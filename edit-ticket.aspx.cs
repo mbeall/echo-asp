bdf9b519141815d95fef58fd4f9b0c2f9dd7d519
Matt Beall <mbeall@starverte.com>
2014-11-15 16:40:20 -0700

    First attempt at ObjectDataSource source code

--- edit-ticket.aspx.cs
+++ edit-ticket.aspx.cs
@@ -7,8 +7,15 @@ using System.Web.UI.WebControls;
 
 public partial class edit_ticket : System.Web.UI.Page
 {
+  private static ObjectDataSource edb = new ObjectDataSource("TicketDBTableAdapters.ticketsTableAdapter","get_ticket");
+
+  public int tkt_id_PK = DataListItem
+  public string tkt_name;
+  public string tkt_desc;
+  public string tkt_created;
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
+      
     }
 }
