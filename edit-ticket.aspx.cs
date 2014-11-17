9750a4a955e9d2b7a1a0bda238629759ed5504e7
Matt Beall <mbeall@starverte.com>
2014-11-17 15:15:33 -0700

    Pre-load validation for edit-ticket.aspx
    Check to make sure user is logged in and ticket is defined

--- edit-ticket.aspx.cs
+++ edit-ticket.aspx.cs
@@ -1,21 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class edit_ticket : System.Web.UI.Page
-{
-  private static ObjectDataSource edb = new ObjectDataSource("TicketDBTableAdapters.ticketsTableAdapter","get_ticket");
-
-  public int tkt_id_PK;
-  public string tkt_name;
-  public string tkt_desc;
-  public string tkt_created;
-
-    protected void Page_Load(object sender, EventArgs e)
-    {
-      
+public partial class edit_ticket : System.Web.UI.Page {
+  protected void Page_Load(object sender, EventArgs e) {
+    if (Session["mod_id"] != null) {
+      if (Session["tkt_id"] == null) {
+        //User is logged in, but no ticket specified
+        Response.Redirect("browse.aspx");
+      }
     }
+    else{
+      //No one is logged in
+      Response.Redirect("index.aspx");
+    }
+  }
 }
