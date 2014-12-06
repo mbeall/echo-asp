0a5de4554fd40a8bdea4fbec09949bf008dc9264
Matt Beall <mbeall@starverte.com>
2014-12-06 15:09:37 -0700

    Cleanup code in login.aspx.cs

--- login.aspx.cs
+++ login.aspx.cs
@@ -11,25 +11,25 @@ Partial Class SignIn
 
       Dim aTable As moderatorsDataTable
 
-    
+
 
       aTable = aAdapter.GetDataByCredentials(Login1.UserName, Login1.Password)
 
-     
+
 
       If aTable.Rows.Count = 1 Then
 
-        
+
 
         Dim intPK As Integer = Convert.ToInt32((aTable.Rows(0).Item("mod_id_pk")))
         Dim strFirstName As String = aTable.Rows(0).Item("mod_first").ToString
         Dim strLastName As String = aTable.Rows(0).Item("mod_last").ToString
 
-        
+
 
         Dim VisitorInfo As New Hashtable
 
-        
+
 
         With VisitorInfo
           .Add("mod_id_pk", intPK)
@@ -37,17 +37,17 @@ Partial Class SignIn
           .Add("mod_last", strLastName)
         End With
 
-       
+
 
         Session.Add("Visitor", VisitorInfo)
 
 
         If Request.QueryString("ReturnURL") <> String.Empty Then
-          
+
 
           FormsAuthentication.RedirectFromLoginPage(strFirstName, False)
         Else
-          
+
           FormsAuthentication.SetAuthCookie(strFirstName, False)
           Response.Redirect("profile.aspx")
         End If
