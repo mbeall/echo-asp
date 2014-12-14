ac0fd2f9375804913259ec85f0b9e60984681af4
Hmturner <hannahmaeturner@gmail.com>
2014-12-14 01:30:49 -0700

    Added runat and hid priority

--- ticket.aspx.cs
+++ ticket.aspx.cs
@@ -9,6 +9,19 @@ public partial class ticket : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+      priority.Visible = false;
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+      if (String.IsNullOrEmpty(Request.QueryString["tkt_id_pk"]))
+      {
 
+        //ticket_name.DefaultMode = (FormViewMode.Insert);
+      }
+      else
+      {
+       // description.DefaultMode = (FormViewMode.Edit);
+        TextBox1.Text = "Thank you for submitting a ticket.";
+      }
     }
 }
