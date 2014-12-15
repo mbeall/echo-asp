197f5f81a3db97406b42a5aa97a28b6004a178d0
Matt Beall <mbeall@starverte.com>
2014-12-14 21:54:32 -0700

    Validation and confirmation for admin ticket page

--- admin/ticket.aspx.cs
+++ admin/ticket.aspx.cs
@@ -9,4 +9,9 @@ public partial class ticket:System.Web.UI.Page {
   protected void Page_Load( object sender, EventArgs e ) {
 
   }
+  protected void fv_edit_ticket_success(object sender, FormViewUpdatedEventArgs e) {
+    if (IsValid) {
+      p_success.Visible = true;
+    }
+  }
 }
